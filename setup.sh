b0b57a7a88cddffaa02908fec25c758ca1ce9103
burpdroid <script1337x@gmail.com>
2021-05-24 01:35:04 +0530

    update installing script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 echo "Updating your system"
-apt-get update
+apt-get update -y
 echo "installing git"
-apt-get install git
+apt-get install git -y
 echo "installing rust"
-apt-get install rust
+apt-get install rust -y
 echo "installing python3"
-apt-get install python
+apt-get install python -y
 echo "installing pip"
-apt-get install python-pip
+apt-get install python-pip -y
 echo "Updating your Burpdroid (beta-version)"
 git clone https://github.com/burpdroid/burpdroid-proxy /data/data/com.termux/files/usr/var/burpdroid-proxy
 echo "setting env veriable"
